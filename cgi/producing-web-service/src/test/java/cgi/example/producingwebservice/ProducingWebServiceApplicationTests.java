19c3c82f6a2c056783f36a9cee3d352490657883
L-M-Fey <63296268+L-M-Fey@users.noreply.github.com>
2021-05-04 13:15:08 +0200

    Add files via upload

---
+++ cgi/producing-web-service/src/test/java/cgi/example/producingwebservice/ProducingWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cgi.example.producingwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
